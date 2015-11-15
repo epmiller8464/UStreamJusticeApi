045e7632c96a1bfa31a3a4a8680ece7c691949d8
Eric Miller <epmiller8464@gmail.com>
2015-11-14 20:35:21 -0600

    unit tests added

--- test/api/controllers/incidentController.spec.js
+++ test/api/controllers/incidentController.spec.js
@@ -8,27 +8,28 @@ var helper = require('../../helpers/TestHelper');
 var config = require('../../../config/environment');
 var controllerName = "incidents";
 var PATH = util.format('/%s/%s/%s', config.api.basePATH, config.api.version,controllerName);
+console.log(PATH);
 
 var count = 5;
 var incidents = helper.getRandomIncidents(count);
 count.should.eql(incidents.length);
 //
-//describe('POST /incident', function () {
-//  incidents.forEach(function(incident, index, array) {
-//    it('should add a new incident', function (done) {
-//
-//      request(server)
-//          .post(PATH)
-//          .type('application/json')
-//          .send(incident)
-//          .end(function (err, res) {
-//            assert.equal(res.statusCode, 201);
-//            var result = res.body;
-//            should.notEqual(result, undefined);
-//            result.should.have.property('_id');
-//            //result.email.should.eql(user.email);
-//            done();
-//          });
-//    });
-//  });
-//});
+describe('POST /incident', function () {
+  incidents.forEach(function(incident, index, array) {
+    it('should add a new incident', function (done) {
+
+      request(server)
+          .post(PATH)
+          .type('application/json')
+          .send(incident)
+          .end(function (err, res) {
+            assert.equal(res.statusCode, 201);
+            var result = res.body;
+            should.notEqual(result, undefined);
+            result.should.have.property('_id');
+            //result.email.should.eql(user.email);
+            done();
+          });
+    });
+  });
+});
