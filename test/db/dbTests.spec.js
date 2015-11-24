24c7a63bccb47d232fafde79bb3f0eb2cb69a0dc
Eric Miller <epmiller8464@gmail.com>
2015-11-24 01:53:35 -0600

    test updates

--- test/db/dbTests.spec.js
+++ test/db/dbTests.spec.js
@@ -139,7 +139,7 @@ describe('Mongoose.Incident', function () {
       });
     });
 
-    var n = c.integer({min: 20000, max: 100000});
+    var n = c.integer({min: 0, max: 20000});
     do {
       //console.log(n);
       it('update an existing incident', function (done) {
